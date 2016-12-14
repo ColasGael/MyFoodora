5075afea83c16295680dd9ab7ca4fd073102bcb3
Colas Gael <gael.colas@student.ecp.fr>
2016-12-14 17:37:13 +0100

    da

---
+++ src/fr/ecp/is1220/MyFoodoraTests/MealTest.java
@@ -0,0 +1,74 @@
+package fr.ecp.is1220.MyFoodoraTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MealTest {
+
+	@Test
+	public void testMealString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMealStringMainDish() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputePrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddDish() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMainDish() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMainDish() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDiscountFactor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/fr/ecp/is1220/MyFoodoraTests/RestaurantTest.java
@@ -0,0 +1,89 @@
+package fr.ecp.is1220.MyFoodoraTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RestaurantTest {
+	
+	@Before
+	public void createRestaurant(){
+		
+	}
+
+	@Test
+	public void testAddDish() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveDish() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindDishByName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMeal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveMeal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindMealByName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddDish2Meal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplaySortedFoodItems() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMenu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMenu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayMenu() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGenericDiscountFactor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSpecialDiscountFactor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAddress() {
+		fail("Not yet implemented");
+	}
+
+}
